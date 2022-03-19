4bb602814c937b17e9a7a31e97ff3fa1409928e1
Jelle <jellehak@gmail.com>
2022-03-19 09:14:44 +0100

    proper sw

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Simple GUI for json-server
+
+# Links
+Service worker implemented according to https://mobiforge.com/design-development/pwa-minimus-a-minimal-pwa-checklist
--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-self.addEventListener("install", function (e) {
-  // eslint-disable-next-line no-console
-  console.log("[ServiceWorker] Install");
-  self.skipWaiting();
+const cacheName = "v2";
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        // './style.css',
+        // './app.js',
+      ]);
+    })
+  );
 });
 
 self.addEventListener("activate", function (e) {
@@ -10,4 +19,19 @@ self.addEventListener("activate", function (e) {
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", function (e) {});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
